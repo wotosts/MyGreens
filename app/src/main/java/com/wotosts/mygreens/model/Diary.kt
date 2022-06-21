4f9085b05f863ebe32ceb92888dc45bc683ebaef
wotosts <tosts522@gmail.com>
2022-06-21 18:57:32 +0900

    plant, diary 클래스 추가

---
+++ app/src/main/java/com/wotosts/mygreens/model/Diary.kt
@@ -0,0 +1,3 @@
+package com.wotosts.mygreens.model
+
+data class Diary(val id: Long, val plant: Plant, val content: String)
---
+++ app/src/main/java/com/wotosts/mygreens/model/Plant.kt
@@ -0,0 +1,36 @@
+package com.wotosts.mygreens.model
+
+import java.util.Date
+
+/**
+ * @property id db 저장용
+ * @property name 식물 이름
+ * @property image 등록시 첨부된 식물 사진
+ * @property description 식물 설명(기타, 특이사항)
+ * @property addedDate 식물 등록일
+ * @property deathDate 식물 죽은날
+ * @property waterLevel 물 양
+ * @property solarLevel 햇빛 양
+ * @property waterPeriod 물 주는 주기(일)
+ * @property nutriPeriod 비료 주는 주기(일)
+ */
+data class Plant(
+    val id: Long,
+    val name: String,
+    val image: String = "",
+    val description: String = "",
+    val addedDate: Date = Date(),
+    val deathDate: Date? = null,
+    val waterLevel: WaterLevel = WaterLevel.MID,
+    val solarLevel: SolarLevel = SolarLevel.MID,
+    val nutriPeriod: Int = 0,
+    val waterPeriod: Int = 0
+)
+
+enum class WaterLevel {
+    LOW, MID, HIGH
+}
+
+enum class SolarLevel {
+    LOW, MID, HIGH
+}
